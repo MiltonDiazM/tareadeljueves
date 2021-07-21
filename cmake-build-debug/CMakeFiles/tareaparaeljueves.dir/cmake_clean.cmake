b02aa9873d48fd9f48f56e6b204b4291d51388c2
Milton Diaz <diazmorcillomilton@gmail.com>
2021-07-21 10:48:17 -0500

    trabajo en clase

---
+++ cmake-build-debug/CMakeFiles/tareaparaeljueves.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tareaparaeljueves.dir/main.cpp.obj"
+  "libtareaparaeljueves.dll.a"
+  "tareaparaeljueves.exe"
+  "tareaparaeljueves.exe.manifest"
+  "tareaparaeljueves.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tareaparaeljueves.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
